23aac1d31c852e28a1711cd8d5a6a17f5a8da9c8
Pierre Fenoll <pierrefenoll@gmail.com>
2019-06-04 15:09:12 +0200

    format + update deps
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- lib/mix/tasks/ecto.dump.schema.ex
+++ lib/mix/tasks/ecto.dump.schema.ex
@@ -14,53 +14,53 @@ defmodule Mix.Tasks.Ecto.Dump.Schema do
   @mysql "mysql"
   @postgres "postgres"
   @template ~s"""
-defmodule <%= app <> "." <> module_name %> do
-  use <%= app %>Web, :model
+  defmodule <%= app <> "." <> module_name %> do
+    use <%= app %>Web, :model
 
-  #IF PRIMARY KEY IS NOT `id` OR YOU HAVE MULTIPLE PRIMARY KEYS -> UNCOMMENT THE FOLLOWING LINE
-  #@primary_key false
-  schema "<%= table %>" do<%= for column <- columns do %>
-    field :<%= String.downcase(elem(column,0)) %>, <%= elem(column, 1) %><%= if elem(column, 2) do %>, primary_key: true<% end %><% end %>
+    #IF PRIMARY KEY IS NOT `id` OR YOU HAVE MULTIPLE PRIMARY KEYS -> UNCOMMENT THE FOLLOWING LINE
+    #@primary_key false
+    schema "<%= table %>" do<%= for column <- columns do %>
+      field :<%= String.downcase(elem(column,0)) %>, <%= elem(column, 1) %><%= if elem(column, 2) do %>, primary_key: true<% end %><% end %>
+    end
   end
-end
-"""
+  """
 
-  @spec parse_repo([term]) :: [Ecto.Repo.t]
+  @spec parse_repo([term]) :: [Ecto.Repo.t()]
   def parse_repo(args) do
     parse_repo(args, [])
   end
 
-  defp parse_repo([key, value|t], acc) when key in ~w(--repo -r) do
-    parse_repo t, [Module.concat([value])|acc]
+  defp parse_repo([key, value | t], acc) when key in ~w(--repo -r) do
+    parse_repo(t, [Module.concat([value]) | acc])
   end
 
-  defp parse_repo([_|t], acc) do
-    parse_repo t, acc
+  defp parse_repo([_ | t], acc) do
+    parse_repo(t, acc)
   end
 
   defp parse_repo([], []) do
-    app = Mix.Project.config[:app]
+    app = Mix.Project.config()[:app]
 
     cond do
       repos = Application.get_env(app, :ecto_repos) ->
         repos
 
-      Map.has_key?(Mix.Project.deps_paths, :ecto) ->
-        Mix.shell.error """
-        warning: could not find repositories for application #{inspect app}.
+      Map.has_key?(Mix.Project.deps_paths(), :ecto) ->
+        Mix.shell().error("""
+        warning: could not find repositories for application #{inspect(app)}.
 
         You can avoid this warning by passing the -r flag or by setting the
         repositories managed by this application in your config/config.exs:
 
-            config #{inspect app}, ecto_repos: [...]
+            config #{inspect(app)}, ecto_repos: [...]
 
         The configuration may be an empty list if it does not define any repo.
-        """
+        """)
+
         []
 
       true ->
         []
-
     end
   end
 
@@ -71,9 +71,9 @@ end
   @doc """
   Ensures the given module is a repository.
   """
-  @spec ensure_repo(module, list) :: Ecto.Repo.t | no_return
+  @spec ensure_repo(module, list) :: Ecto.Repo.t() | no_return
   def ensure_repo(repo, args) do
-    Mix.Task.run "loadpaths", args
+    Mix.Task.run("loadpaths", args)
 
     unless "--no-compile" in args do
       Mix.Project.compile(args)
@@ -84,31 +84,39 @@ end
         if function_exported?(repo, :__adapter__, 0) do
           repo
         else
-          Mix.raise "Module #{inspect repo} is not an Ecto.Repo. " <>
-                    "Please configure your app accordingly or pass a repo with the -r option."
+          Mix.raise(
+            "Module #{inspect(repo)} is not an Ecto.Repo. " <>
+              "Please configure your app accordingly or pass a repo with the -r option."
+          )
         end
+
       {:error, error} ->
-        Mix.raise "Could not load #{inspect repo}, error: #{inspect error}. " <>
-                  "Please configure your app accordingly or pass a repo with the -r option."
+        Mix.raise(
+          "Could not load #{inspect(repo)}, error: #{inspect(error)}. " <>
+            "Please configure your app accordingly or pass a repo with the -r option."
+        )
     end
   end
 
   @doc """
   Ensures the given repository is started and running.
   """
-  @spec ensure_started(Ecto.Repo.t, Keyword.t) :: {:ok, pid, [atom]} | no_return
+  @spec ensure_started(Ecto.Repo.t(), Keyword.t()) :: {:ok, pid, [atom]} | no_return
   def ensure_started(repo, opts) do
     {:ok, _} = Application.ensure_all_started(:ecto)
     {:ok, apps} = repo.__adapter__.ensure_all_started(repo, :temporary)
 
     pool_size = Keyword.get(opts, :pool_size, 1)
+
     case repo.start_link(pool_size: pool_size) do
       {:ok, pid} ->
         {:ok, pid, apps}
+
       {:error, {:already_started, _pid}} ->
         {:ok, nil, apps}
+
       {:error, error} ->
-        Mix.raise "Could not start repo #{inspect repo}, error: #{inspect error}"
+        Mix.raise("Could not start repo #{inspect(repo)}, error: #{inspect(error)}")
     end
   end
 
@@ -116,87 +124,123 @@ end
   Returns the private repository path relative to the source.
   """
   def source_repo_priv(repo) do
-    repo.config()[:priv] || "priv/#{repo |> Module.split |> List.last |> Macro.underscore}"
+    repo.config()[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
   end
 
   @doc false
   def run(args) do
     repos = parse_repo(args)
 
-    Enum.each repos, fn repo ->
+    Enum.each(repos, fn repo ->
       ensure_repo(repo, [])
       ensure_started(repo, [])
 
-      driver = repo.__adapter__
-        |> Atom.to_string
-        |> String.downcase
+      driver =
+        repo.__adapter__
+        |> Atom.to_string()
+        |> String.downcase()
         |> String.split(".")
-        |> List.last
-      IO.puts driver
+        |> List.last()
+
+      IO.puts(driver)
 
       generate_models(driver, repo)
-    end
+    end)
   end
 
   def generate_models(driver, repo) when driver == @mysql do
     with config <- repo.config,
-      true <- Keyword.keyword?(config),
-      {:ok, database} <- Keyword.fetch(config, :database)
-    do
-        {:ok, result} = repo.query("SELECT table_name FROM information_schema.tables WHERE table_schema = '#{database}'")
-        Enum.each result.rows, fn [table] ->
-          {:ok, description} = repo.query("SELECT COLUMN_NAME, DATA_TYPE, CASE WHEN `COLUMN_KEY` = 'PRI' THEN '1' ELSE NULL END AS primary_key FROM information_schema.columns WHERE table_name= '#{table}' and table_schema='#{database}'")
-          columns = Enum.map description.rows, fn [column_name, column_type, is_primary] ->
+         true <- Keyword.keyword?(config),
+         {:ok, database} <- Keyword.fetch(config, :database) do
+      {:ok, result} =
+        repo.query(
+          "SELECT table_name FROM information_schema.tables WHERE table_schema = '#{database}'"
+        )
+
+      Enum.each(result.rows, fn [table] ->
+        {:ok, description} =
+          repo.query(
+            "SELECT COLUMN_NAME, DATA_TYPE, CASE WHEN `COLUMN_KEY` = 'PRI' THEN '1' ELSE NULL END AS primary_key FROM information_schema.columns WHERE table_name= '#{
+              table
+            }' and table_schema='#{database}'"
+          )
+
+        columns =
+          Enum.map(description.rows, fn [column_name, column_type, is_primary] ->
             {column_name, get_type(column_type), is_primary}
-          end
-          
-          content = EEx.eval_string(@template, [
-            app: Mix.Project.config[:app] |> Atom.to_string |> String.capitalize,
+          end)
+
+        content =
+          EEx.eval_string(@template,
+            app: Mix.Project.config()[:app] |> Atom.to_string() |> String.capitalize(),
             table: table,
             module_name: to_camelcase(table),
             columns: columns
-          ])
-          write_model(table, content)
-        end
+          )
+
+        write_model(table, content)
+      end)
     end
   end
 
-
   def generate_models(driver, repo) when driver == @postgres do
-    {:ok, result} = repo.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-    Enum.each result.rows, fn [table] ->
-      {:ok, primary_keys} = repo.query("SELECT c.column_name FROM information_schema.table_constraints tc JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema AND tc.table_name = c.table_name AND ccu.column_name = c.column_name where constraint_type = 'PRIMARY KEY' and tc.table_name = '#{table}'")
-      {:ok, description} = repo.query("SELECT column_name, data_type FROM information_schema.columns WHERE table_name ='#{table}'")
-      columns = Enum.map description.rows, fn [column_name, column_type] ->
-        found = Enum.find(List.flatten(primary_keys.rows), {:not_found}, fn x -> x == column_name end)
-        case found do
-          {:not_found} -> {column_name, get_type(List.first(String.split(String.downcase(column_type)))), nil}
-          _ -> {column_name, get_type(column_type), true}
-        end
-      end
-      content = EEx.eval_string(@template, [
-        app: Mix.Project.config[:app] |> Atom.to_string |> String.capitalize,
-        table: table,
-        module_name: to_camelcase(table),
-        columns: columns
-      ])
+    {:ok, result} =
+      repo.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+
+    Enum.each(result.rows, fn [table] ->
+      {:ok, primary_keys} =
+        repo.query(
+          "SELECT c.column_name FROM information_schema.table_constraints tc JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema AND tc.table_name = c.table_name AND ccu.column_name = c.column_name where constraint_type = 'PRIMARY KEY' and tc.table_name = '#{
+            table
+          }'"
+        )
+
+      {:ok, description} =
+        repo.query(
+          "SELECT column_name, data_type FROM information_schema.columns WHERE table_name ='#{
+            table
+          }'"
+        )
+
+      columns =
+        Enum.map(description.rows, fn [column_name, column_type] ->
+          found =
+            Enum.find(List.flatten(primary_keys.rows), {:not_found}, fn x -> x == column_name end)
+
+          case found do
+            {:not_found} ->
+              {column_name, get_type(List.first(String.split(String.downcase(column_type)))), nil}
+
+            _ ->
+              {column_name, get_type(column_type), true}
+          end
+        end)
+
+      content =
+        EEx.eval_string(@template,
+          app: Mix.Project.config()[:app] |> Atom.to_string() |> String.capitalize(),
+          table: table,
+          module_name: to_camelcase(table),
+          columns: columns
+        )
+
       write_model(table, content)
-    end
+    end)
   end
 
   def generate_models(driver, repo) do
-    IO.puts "#{driver} is not yet implemented inspect #{repo}"
+    IO.puts("#{driver} is not yet implemented inspect #{repo}")
   end
 
   defp write_model(table, content) do
-    app_name = Mix.Project.config[:app] |> Atom.to_string
+    app_name = Mix.Project.config()[:app] |> Atom.to_string()
     filename = "lib/" <> app_name <> "_web/models/" <> table <> ".ex"
-    File.rm filename
+    File.rm(filename)
     {:ok, file} = File.open(filename, [:write])
-    IO.binwrite file, content
+    IO.binwrite(file, content)
     File.close(file)
 
-    IO.puts "\e[0;35m  #{filename} was generated"
+    IO.puts("\e[0;35m  #{filename} was generated")
   end
 
   defp to_camelcase(table_name) do
@@ -207,19 +251,35 @@ end
     case row do
       type when type in ["int", "integer", "bigint", "mediumint", "smallint", "tinyint"] ->
         ":integer"
-      type when type in ["varchar", "text", "char", "year", "mediumtext", "longtext", "tinytext", "character"] ->
+
+      type
+      when type in [
+             "varchar",
+             "text",
+             "char",
+             "year",
+             "mediumtext",
+             "longtext",
+             "tinytext",
+             "character"
+           ] ->
         ":string"
+
       type when type in ["decimal", "float", "double", "real"] ->
         ":float"
+
       type when type in ["boolean", "bit", "bit varying"] ->
         ":boolean"
+
       type when type in ["datetime", "timestamp", "date", "time"] ->
         "Ecto.DateTime"
+
       type when type in ["blob"] ->
-        ":binary"     
-      type -> 
-        IO.puts "\e[0;31m  #{type} is not supported ... Fallback to :string"
-        ":string" 
+        ":binary"
+
+      type ->
+        IO.puts("\e[0;31m  #{type} is not supported ... Fallback to :string")
+        ":string"
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -2,35 +2,44 @@ defmodule EctoGenerator.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ecto_generator,
-     version: "9.0.0",
-     elixir: "~> 1.3",
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
-  
-  defp deps do
     [
-       {:ex_doc, ">= 0.0.0", only: :dev},
+      app: :ecto_generator,
+      version: "10.0.0",
+      elixir: "~> 1.8",
+      package: package(),
+      aliases: aliases(),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
-  
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp aliases,
+    do: [
+      compile: ["format", "compile"]
+    ]
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
   defp package do
-    [# These are the default files included in the package
+    [
       name: :ecto_generator,
       description: "Generate Ecto schemas from existing database in Phoenix - Elixir",
       files: ["lib", "config", "mix.exs", "README*"],
       maintainers: ["Bagu Alexandru Bogdan"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/alexandrubagu/ecto_generator", "Docs" => "https://github.com/alexandrubagu/ecto_generator", "Website" => "http://www.alexandrubagu.info" }]
+      links: %{
+        "GitHub" => "https://github.com/alexandrubagu/ecto_generator",
+        "Docs" => "https://github.com/alexandrubagu/ecto_generator",
+        "Website" => "http://www.alexandrubagu.info"
+      }
+    ]
   end
 end
